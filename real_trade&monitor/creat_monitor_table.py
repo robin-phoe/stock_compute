bcd07a5881f561b9578f057104a6232c97ca0389
robin <812121052@qq.com>
2021-06-25 12:30:04 +0800

    '修改create'

--- real_trade&monitor/creat_monitor_table.py
+++ real_trade&monitor/creat_monitor_table.py
@@ -36,6 +36,11 @@ def creat_sql(trade_date):
                                    'WHERE trade_date = "{0}"  AND monitor = 1 AND grade > 10000 AND stock_id not like "688%" ' \
                                      ' AND stock_name NOT LIKE "ST%" AND stock_name NOT LIKE "*ST%" ' \
                                      ''.format(trade_date) #AND grade >= 0 分数尚未完善
+    table_dict['remen_boxin'] ='SELECT stock_id,stock_name,grade,"remen_boxin" ' \
+                                   'FROM remen_boxin ' \
+                                   'WHERE trade_date = "{0}"  AND monitor = 1 AND grade > 10000 AND stock_id not like "688%" ' \
+                                     ' AND stock_name NOT LIKE "ST%" AND stock_name NOT LIKE "*ST%" ' \
+                                     ''.format(trade_date) #AND grade >= 0 分数尚未完善
 def sel_lastest_day():
     cursor = db.cursor()  # 使用cursor()方法获取用于执行SQL语句的游标
     sql  = "select max(trade_date) from stock_trade_data"
