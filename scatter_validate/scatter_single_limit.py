a7ff776155f4eaa834319fa91cae89e97529f89c
robin <812121052@qq.com>
2021-11-03 00:10:27 +0800

    修改散点图校验

--- scatter_validate/scatter_single_limit.py
+++ scatter_validate/scatter_single_limit.py
@@ -11,29 +11,31 @@ class pic:
         self.monitor_df = None
         self.n_monitor_df = None
     def select(self):
-        sql = "select s.stock_id,s.high_price,s.low_price,s.increase,l.grade,l.monitor from stock_trade_data s " \
+        sql = "select s.trade_date,s.stock_id,s.high_price,s.low_price,s.increase,l.grade,l.monitor,l.type from stock_trade_data s " \
               " LEFT JOIN limit_up_single l " \
               " ON s.trade_code = l.trade_code " \
-              " where s.trade_date >='{0}' and s.trade_date <='{1}'".format(self.start_date,self.end_date)
+              " where s.trade_date >='{0}' and s.trade_date <='{1}' and l.type = 'standard'".format(self.start_date,self.end_date)
         self.sel_df = pub_uti_a.creat_df(sql)
+        self.sel_df.to_csv('record_sel.csv')
     def deal_data(self):
         df_group =self.sel_df.groupby('stock_id',as_index =False)
-        self.sel_df['inc_1'] = df_group['increase'].shift(-1)
-        self.sel_df['inc_2'] = df_group['increase'].shift(-2)
+        self.sel_df['inc_1'] = df_group['increase'].shift(1)
+        self.sel_df['inc_2'] = df_group['increase'].shift(2)
         # self.sel_df['after_1_high'] = df_group['high_price'].shift(-1)
         # self.sel_df['after_2_high'] = df_group['high_price'].shift(-2)
         # self.sel_df['after_2_mid'] = (df_group['high_price'].shift(-2) + df_group['low_price'].shift(-2))/2
         self.sel_df.fillna(0,inplace = True)
         # print('self.sel_df:', self.sel_df)
-        self.monitor_df = self.sel_df[self.sel_df.monitor == 1]
-        self.n_monitor_df = self.sel_df[self.sel_df.monitor != 1]
+        self.monitor_df = self.sel_df[(self.sel_df.monitor == 1) & (self.sel_df.inc_1 >=2.5)]
+        self.monitor_df.to_csv('monitor_df.csv')
+        self.n_monitor_df = self.sel_df[self.sel_df.monitor != 1 & (self.sel_df.inc_1 >=2.5)]
     def show_pic(self):
         self.select()
         self.deal_data()
-        ax = self.sel_df.plot.scatter(x='grade', y='inc_2',color = 'Green',label = 'g1')
-        self.n_monitor_df.plot.scatter(x='grade', y='inc_2', color='Blue', label='g2',ax = ax)
+        # ax = self.sel_df.plot.scatter(x='grade', y='inc_2',color = 'Green',label = 'g1')
+        self.n_monitor_df.plot.scatter(x='grade', y='inc_2')
         plt.show()
 if __name__ == '__main__':
-    p =pic('2020-10-11','2021-10-21')
+    p =pic('2021-06-20','2021-10-31')
     p.show_pic()
 
