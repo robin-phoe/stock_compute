4a068566d3f774d61f9f7853b2f43ba63d7e3683
robin <812121052@qq.com>
2022-04-04 18:02:15 +0800

    统计大拉升修改

--- strategy/tongji_dalasheng.py
+++ strategy/tongji_dalasheng.py
@@ -10,17 +10,17 @@ from multiprocessing import Pool
 import sys
 import os
 sys.path.append(os.path.join(os.path.dirname(os.getcwd()),"config"))
-from readconfig import read_config
-
+from ·312·readconfig import read_config
+import pub_uti_a
 
 logging.basicConfig(level=logging.DEBUG, filename='../log/tongji_lasheng.py.log', filemode='w',
                     format='%(asctime)s-%(levelname)5s: %(message)s')
-def save(db,trade_code,stock_id,stock_name,trade_date,count):
+def save(db,trade_code,stock_id,stock_name,arise_date,count):
     cursor = db.cursor()
-    sql = "insert into tongji_dalasheng(trade_code,trade_date,stock_id,stock_name,zhangting_count) \
+    sql = "insert into tongji_dalasheng(trade_code,arise_date,stock_id,stock_name,zhangting_count) \
         values('{0}','{1}','{2}','{3}','{4}') " \
-          "ON DUPLICATE KEY UPDATE trade_code='{0}',trade_date='{1}',stock_id='{2}',stock_name='{3}'," \
-          "zhangting_count ='{4}' ".format(trade_code,trade_date,stock_id,stock_name,count)
+          "ON DUPLICATE KEY UPDATE trade_code='{0}',arise_date='{1}',stock_id='{2}',stock_name='{3}'," \
+          "zhangting_count ='{4}' ".format(trade_code,arise_date,stock_id,stock_name,count)
     print('sql:', sql)
     cursor.execute(sql)
     try:
@@ -100,13 +100,72 @@ def run(start_t,end_t):
     p.close()
     p.join()
     print('All subprocesses done.')
+##补充量异动统计
+class add_value_info:
+    def __init__(self):
+        self.save = pub_uti_a.save()
+    def select_buffer(self):
+        sql = "SELECT D.trade_code,D.stock_id,D.stock_name,D.zhangting_count," \
+              " D.arise_date,S.trade_date,S.value_abnormal " \
+              " FROM tongji_dalasheng D " \
+              "LEFT JOIN stock_trade_data S " \
+              "ON D.stock_id = S.stock_id "
+        self.df = pub_uti_a.creat_df(sql,ascending=True)
+        stock_id_set = set(self.df['stock_id'].to_list())
+        for id in stock_id_set:
+            single_df = self.df[self.df['stock_id'] == id]
+            arise_date_set = set(single_df['arise_date'].to_list())
+            for lasheng_date in arise_date_set:
+                single_df_sun = single_df[single_df['arise_date'] == lasheng_date]
+                s = stock(single_df_sun,lasheng_date)
+                save_sql = "update tongji_dalasheng set count_single= '{}',lastest_info = '{}' " \
+                           "WHERE trade_code = '{}'".format(s.count_single,s.lastest_info,s.trade_code)
+                self.save.add_sql(save_sql)
+                print(s.stock_name,s.arise_date,s.zhangting_count,s.count_single,s.lastest_info)
+        self.save.commit()
+class stock:
+    def __init__(self,single_df,arise_date):
+        self.trade_code = ''
+        self.stock_id = ''
+        self.stock_name = ''
+        self.zhangting_count = 0
+        self.count_single= 0
+        self.lastest_info = 'N'
+        self.df = single_df
+        self.arise_date = str(arise_date)
+        self.info_tup = ('single','single2','hat','highland')
+        self.deal_data()
+    def deal_data(self):
+        self.df.reset_index(inplace=True,drop=True)
+        self.stock_id = self.df.loc[0,'stock_id']
+        self.stock_name = self.df.loc[0,'stock_name']
+        self.trade_code = self.df.loc[0,'trade_code']
+        self.zhangting_count = self.df.loc[0,'zhangting_count']
+        index_list = self.df[self.df['trade_date'] == self.arise_date].index.to_list()
+        if len(index_list) == 1:
+            arise_index = index_list[0]
+            if arise_index <20: #新上市
+                self.lastest_info = 'init'
+                return 0
+        else:
+            print('ERROR:{} {} has error,when finding a index. {}'.format(self.stock_id,self.stock_name,len(index_list)))
+            return 0
+        for ind in range(arise_index-1,-1,-1):
+            if self.df.loc[ind,'value_abnormal'] in self.info_tup:
+                self.lastest_info = self.df.loc[ind,'value_abnormal']
+                self.count_single +=1
+            else:
+                break
+
 
 if __name__ == '__main__':
-    end_t ='2021-05-01'#None#'2021-02-01' #'2021-01-20'
-    start_t= '2018-01-01'
-
-    time1 = datetime.datetime.now()
-    h_tab = '1'
-    main(h_tab,start_t,end_t)
+    # end_t ='2021-12-31'#None#'2021-02-01' #'2021-01-20'
+    # start_t= '2018-01-01'
+    #
+    # time1 = datetime.datetime.now()
+    # # h_tab = '1'
+    # # main(h_tab,start_t,end_t)
     # run(start_t,end_t)
-    print('time_delta:', datetime.datetime.now() - time1)
+    # print('time_delta:', datetime.datetime.now() - time1)
+
+    add_value_info().select_buffer()
