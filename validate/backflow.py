b28463a9403405ba017e1da8edecb4dd21b98141
robin <812121052@qq.com>
2022-06-01 00:27:02 +0800

    增加部分回测规划

--- validate/backflow.py
+++ validate/backflow.py
@@ -6,23 +6,45 @@
 import pub_uti_a
 import pandas as pd
 
-class setting:
+class base_config:
     mode = 'ratio'#limit_cash:限定资金；ratio:比值模式
     timeout = 3
+    stop_profit = 0.05 * 100#
+    stop_loss = 0.05 * 100
 
 init_capital = 1000000
 start_time = ""
 end_time = ""
+
+#信号
+class single:
+    code = None
+    name = None
+    grade = None
+    conf = base_config()
 #创建信号集合
 class signal_hub:
     def __init__(self,start_date,end_date):
-        #trade_code,grade
-        sql= "select trade_code,grade" \
-                  " from limit_up_single " \
-             " where trade_date >= '{}' and trade_date <='{}' and grade > 0"
-        self.df = pub_uti_a.creat_df(sql,ascending=True)
-    def run(self):
-        return self.df
+        self.start_date = start_date
+        self.end_date = end_date
+        self.df = None
+
+    def select_single(self):
+        sql = "select trade_code,grade" \
+              " from limit_up_single " \
+              " where trade_date >= '{}' and trade_date <='{}' and grade > 0".format(self.start_date,self.end_date)
+        self.df = pub_uti_a.creat_df(sql, ascending=True)
+    '''
+    def create_info_df(self):
+        self.select_signal()
+        self.select_market()
+        self.info_df = pd.merge(self.market_df,self.signal_df,on='trade_code')
+        self.info_df['grade'].fillna(-9999,inplace=True)
+    #创建当日信号集（按分数倒序）
+    def create_today_signal(self,date):
+        today_signal = self.info_df[self.info_df['trade_date'] == date & self.info_df['grade'] >= self.start_grade]
+        return today_signal
+    '''
 
 class position:
     def __init__(self,stock_id,stock_name,qty,buy_price):
@@ -44,6 +66,7 @@ class main:
         self.single_day_trade = 0
         self.create_info_df()
         self.start_grade = 0
+        self.trade_date_list = []
 
     #创建时段内时序信号集
     def select_signal(self):
@@ -54,22 +77,12 @@ class main:
               " from stock_trade_data " \
               "where trade_date >= '{}' and trade_date <= '{}' ".format(self.start_date,self.end_date)
         self.market_df = pub_uti_a.creat_df(sql)
-    def create_info_df(self):
-        self.select_signal()
-        self.select_market()
-        self.info_df = pd.merge(self.market_df,self.signal_df,on='trade_code')
-        self.info_df['grade'].fillna(-9999,inplace=True)
-    #创建当日信号集（按分数倒序）
-    def create_today_signal(self,date):
-        today_signal = self.info_df[self.info_df['trade_date'] == date & self.info_df['grade'] >= self.start_grade]
-        return today_signal
 
     #建仓操作
     def buy_operate(self,date):
         today_signal = self.create_today_signal(date)
         #资金分配
         #per_s_capital =
-
         for stock in today_signal:
             if stock in self.position_buffer:
                 continue
@@ -77,9 +90,17 @@ class main:
     #平仓操作
     def sell_operate(self):
         pass
+    #创建交易日列表
+    def create_tradedate_list(self):
+        sql = "select distinct trade_date from stock_trade_data where trade_date >= '{}' and trade_date <= '{}'".format(
+            self.start_date,self.end_date
+        )
+        trade_data_df = pub_uti_a.creat_df(sql,ascending=True)
+        self.trade_date_list = trade_data_df['trade_date'].to_list()
     #遍历日期，触发建仓平仓
     def run(self):
-        for date in self.datelist:
+        self.create_tradedate_list()
+        for date in self.trade_date_list:
             self.single_day_trade = 0
             #处理买信号
             self.buy_operate(date)
