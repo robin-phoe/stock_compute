0dbcf6049525d6bca34427a610b1722a9ae9128f
robin <812121052@qq.com>
2023-01-05 00:02:00 +0800

    修改配置读取

--- config/readconfig.py
+++ config/readconfig.py
@@ -4,6 +4,12 @@ def read_config(param):
         with open('../config/db_config.json','r') as f:
             config_param = json.load(f)
         return config_param
+db_config= {
+  "host":"192.168.1.99",
+  "user": "user1",
+  "password": "Zzl08382020",
+  "database": "stockdb"
+}
 if __name__ == '__main__':
     config_param = read_config('db_config')
     print(config_param)
